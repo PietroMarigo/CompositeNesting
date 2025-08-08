1add253da92c93d8fea900b1e1a2ee7e082ff204
PietroMarigo <72839890+PietroMarigo@users.noreply.github.com>
2025-08-08 19:00:36 +0200

    feat: bundle backend with electron

--- frontend/electron/main.js
+++ frontend/electron/main.js
@@ -1,5 +1,23 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const { spawn } = require('child_process');
+
+let backendProcess;
+
+function startBackend() {
+  const jarPath = app.isPackaged
+    ? path.join(process.resourcesPath, 'backend', 'backend.jar')
+    : path.join(__dirname, '..', '..', 'backend', 'target', 'backend-0.0.1-SNAPSHOT.jar');
+
+  backendProcess = spawn('java', ['-jar', jarPath], {
+    stdio: 'inherit',
+    cwd: app.isPackaged ? process.resourcesPath : undefined,
+  });
+
+  backendProcess.on('close', (code) => {
+    console.log(`Backend exited with code ${code}`);
+  });
+}
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -10,10 +28,15 @@ function createWindow() {
     },
   });
 
-  win.loadURL('http://localhost:5173');
+  if (app.isPackaged) {
+    win.loadFile(path.join(__dirname, '..', 'dist', 'index.html'));
+  } else {
+    win.loadURL('http://localhost:5173');
+  }
 }
 
 app.whenReady().then(() => {
+  startBackend();
   createWindow();
 
   app.on('activate', () => {
@@ -23,4 +46,10 @@ app.whenReady().then(() => {
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('before-quit', () => {
+  if (backendProcess) {
+    backendProcess.kill();
+  }
 });
